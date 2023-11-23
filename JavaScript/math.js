9b56b21c91ddc734fd6c1549ff5b1cce9176c604
Rafiul-Coder <mdrafiulislam06@gmail.com>
2023-11-23 23:31:54 +0600

    kk

--- JavaScript/math.js
+++ JavaScript/math.js
@@ -1,4 +1,9 @@
-const addFunc=(a,b)=>{
+const addFunc=(a,b,c)=>{
     return a+b
 }
-console.log(addFunc(2,3))
+console.log(addFunc(2,3,4))
+
+const mathFun=(a)=>{
+  return a*a
+}
+console.log(mathFun(3))
